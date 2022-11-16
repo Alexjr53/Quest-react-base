d54260294187abf082e0a91bd7c5693867d8bef9
Alexsandro <alexsandrojunior53@gmail.com>
2022-11-16 06:28:17 -0300

    código refatorado

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
       <div className='container'>
         <Paragraph color='darkblue' text ='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae reprehenderit aspernatur ea, facere non nemo placeat repellat natus similique voluptatum repellendus inventore voluptates, dolorem eligendi ipsam nihil labore omnis. Aliquid?'/>
-
         <Button label='Baixar arquivo'/>
       </div>
   );
